2b6676b889c3934beb4ca5da5e100119d2fe3a13
Julia Eskew <jeskew@edx.org>
2020-05-13 12:41:25 -0400

    Modifications of the cookiecutter files.

--- prefect_utils/__init__.py
+++ prefect_utils/__init__.py
@@ -1,5 +1,5 @@
-"""Top-level package for prefect-utils."""
+"""
+Top-level package for prefect-utils.
+"""
 
-__author__ = """Julia Eskew"""
-__email__ = 'jeskew@edx.org'
 __version__ = '0.1.0'
--- prefect_utils/cli.py
+++ prefect_utils/cli.py
@@ -1,11 +1,15 @@
-"""Console script for prefect_utils."""
+"""
+Console script for prefect_utils.
+"""
 import sys
 import click
 
 
 @click.command()
 def main(args=None):
-    """Console script for prefect_utils."""
+    """
+    Console script for prefect_utils.
+    """
     click.echo("Replace this message by putting your code into "
                "prefect_utils.cli.main")
     click.echo("See click documentation at https://click.palletsprojects.com/")
