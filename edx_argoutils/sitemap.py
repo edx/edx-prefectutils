9cddbd20a0437b98f8776545d8a66450db3ad58c
Muhammad Usama <muhammad.usama@arbisoft.com>
2024-11-07 17:41:59 +0500

    refactor: refactored sitemap.py

--- edx_argoutils/sitemap.py
+++ edx_argoutils/sitemap.py
@@ -1,19 +1,16 @@
 """
-Tasks for pulling sitemap data.
+Functions for pulling sitemap data.
 """
 
 import json
 import xml.etree.ElementTree as ET
 from os.path import basename, join, splitext
 from urllib.parse import urlparse
-
-import prefect
+import boto3
 import requests
-from prefect import task
-from prefect.tasks.aws import s3
+from common import getdate
 
 
-@task
 def fetch_sitemap_urls(sitemap_index_url: str) -> str:
     """
     Fetches a list of sitemap urls by parsing sitemap index file.
@@ -25,12 +22,11 @@ def fetch_sitemap_urls(sitemap_index_url: str) -> str:
     return sitemap_urls
 
 
-@task
 def fetch_sitemap(sitemap_url: str):
     """
     Fetches sitemap data from a given sitemap URL.
     """
-    scraped_at = str(prefect.context.date)
+    scraped_at = getdate(None)
     r = requests.get(sitemap_url)
     sitemap_xml = r.text
     tree = ET.fromstring(sitemap_xml)
@@ -44,17 +40,20 @@ def fetch_sitemap(sitemap_url: str):
     return sitemap_filename, json.dumps(sitemap_json)
 
 
-@task
 def write_sitemap_to_s3(sitemap_data: str, s3_bucket: str, s3_path: str):
     """
     Writes sitemap data in JSON format to S3.
     """
     filename, sitemap_json = sitemap_data
-    date_path = f'{prefect.context.today}/{filename}.json'
-    s3_key = join(s3_path, date_path)
-    s3.S3Upload(bucket=s3_bucket).run(
-        sitemap_json,
-        key=s3_key,
+    today = getdate(None)
+    date_path = f'{today}/{filename}.json'
+    s3_key = f'{s3_path}/{date_path}'
+    s3_client = boto3.client('s3')
+    s3_client.put_object(
+        Bucket=s3_bucket,
+        Key=s3_key,
+        Body=sitemap_json,
+        ContentType='application/json'
     )
 
     return date_path
